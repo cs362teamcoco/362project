9ec559170bb69244c4c23a9e2ac182aa8c5b5d28
Eric Kasnetsis <basstromboneplayer@yahoo.com>
2011-02-08 17:16:02 -0800

    added html to top

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 
 ?>
 <!DOCTYPE html>
+<html>
 <head>
 	<meta http-equiv="Content-type" content="text/html; charset=utf-8">
 	<title>CS362 Project</title>
-	
 </head>
 <body>
 	<h1>CS362 Project</h1>
