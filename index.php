b0330c7512932c467f6fbbc29398d8c9a41b0fdc
Eric Kasnetsis <basstromboneplayer@yahoo.com>
2011-04-21 17:01:42 -0700

    added search form

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
 	<title>CS362 Project</title>
 </head>
 <body>
-	<h1>CS362 Project</h1>
-</body>
+	<form action="search.php" method="POST" accept-charset="utf-8">
+		<label for="search">Search</label><input type="text" name="search" value="" id="search">
+		<p><input type="submit" value="Search"></p>
+	</form></body>
 </html>
